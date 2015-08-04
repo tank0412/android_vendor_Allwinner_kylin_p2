cdaaa647e4d0aee99ad1a3baf44ceb5b8cde91e2
Roman <romabukhtiarov@gmail.com>
2015-08-04 09:51:37 +0300

    Initial release
    Using blob files from leaked Lollipop

---
+++ README.md
@@ -0,0 +1,3 @@
+It is a device tree for onda V989.
+vemdor/onda/v989
+Guys, i need your help. Please pull request!
---
+++ V989Config.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH := vendor/onda/v989
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/system/bin/p2p_supplicant_overlay.conf:system/bin/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/system/bin/wpa_supplicant:system/bin/wpa_supplicant \
+$(LOCAL_PATH)/system/bin/wpa_supplicant.conf:system/bin/wpa_supplicant.conf \
+$(LOCAL_PATH)/system/bin/wpa_supplicant_overlay.conf:system/bin/wpa_supplicant_overlay.conf \
+$(LOCAL_PATH)/system/etc/vold.fstab:system/etc/vold.fstab \
+$(LOCAL_PATH)/system/etc/bluetooth/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
+$(LOCAL_PATH)/system/etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+$(LOCAL_PATH)/system/etc/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
+$(LOCAL_PATH)/system/etc/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
+$(LOCAL_PATH)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+$(LOCAL_PATH)/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+$(LOCAL_PATH)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+$(LOCAL_PATH)/system/lib/libEGL.so:system/lib/libEGL.so \
+$(LOCAL_PATH)/system/lib/libGLES_trace.so:system/lib/libGLES_trace.so \
+$(LOCAL_PATH)/system/lib/libOmxCore.so:system/lib/libOmxCore.so \
+$(LOCAL_PATH)/system/lib/libOmxVdec.so:system/lib/libOmxVdec.so \
+$(LOCAL_PATH)/system/lib/libOmxVenc.so:system/lib/libOmxVenc.so \
+$(LOCAL_PATH)/system/lib/libGLESv1_CM.so:system/lib/libGLESv1_CM.so \
+$(LOCAL_PATH)/system/lib/libGLESv2.so:system/lib/libGLESv2.so \
+$(LOCAL_PATH)/system/lib/libGLESv3.so:system/lib/libGLESv3.so \
+$(LOCAL_PATH)/system/lib/libpower.so:system/lib/libpower.so \
+$(LOCAL_PATH)/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+$(LOCAL_PATH)/system/lib/hw/audio.primary.kylin.so:system/lib/hw/audio.primary.kylin.so \
+$(LOCAL_PATH)/system/lib/hw/camera.kylin.so:system/lib/hw/camera.kylin.so \
+$(LOCAL_PATH)/system/lib/hw/gralloc.default.so:system/lib/hw/gralloc.default.so \
+$(LOCAL_PATH)/system/lib/hw/hwcomposer.kylin.so:system/lib/hw/hwcomposer.kylin.so \
+$(LOCAL_PATH)/system/lib/hw/sensors.kylin.so:system/lib/hw/sensors.kylin.so \
+$(LOCAL_PATH)/system/usr/idc/gt9xx.idc:system/usr/idc/gt9xx.idc \
+$(LOCAL_PATH)/system/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+$(LOCAL_PATH)/system/usr/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+$(LOCAL_PATH)/system/usr/keylayout/sunxi-ir.kl:system/usr/keylayout/sunxi-ir.kl \
+$(LOCAL_PATH)/system/usr/keylayout/sunxi-keyboard.kl:system/usr/keylayout/sunxi-keyboard.kl 
+
+
+
