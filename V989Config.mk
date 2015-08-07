4368a53568036715433696e26159c8ae19adff27
Roman <romabukhtiarov@gmail.com>
2015-08-07 14:51:12 +0300

    vold.fstab is deleted
    Lollipop do not need this

--- V989Config.mk
+++ V989Config.mk
@@ -4,7 +4,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/system/bin/wpa_supplicant:system/bin/wpa_supplicant \
 $(LOCAL_PATH)/system/bin/wpa_supplicant.conf:system/bin/wpa_supplicant.conf \
 $(LOCAL_PATH)/system/bin/wpa_supplicant_overlay.conf:system/bin/wpa_supplicant_overlay.conf \
-$(LOCAL_PATH)/system/etc/vold.fstab:system/etc/vold.fstab \
 $(LOCAL_PATH)/system/etc/powervr.ini:system/etc/powervr.ini \
 $(LOCAL_PATH)/system/etc/bluetooth/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
 $(LOCAL_PATH)/system/etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
